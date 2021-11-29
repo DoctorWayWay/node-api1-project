cc2ed4f7e2a186bd4d239b6f362264d28ed52449
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-29 14:28:37 -0800

    created a post user endpoint

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // ===== IMPORTS =====
 const express = require("express")
+const Users = require("./users/model")
 
 // ===== INSTANCE OF EXPRESS APP =====
 const server = express()
@@ -13,6 +14,28 @@ server.get("/hello", (req, res) => {
   res.json({
     "message": "Hello from port 9000"
   })
+})
+
+server.post("/api/users", async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    // Validating if request body is appropriate
+    if (!name || !bio) {
+      res.status(400).json({
+        "message": "provide name and bio"
+      })
+    } else {
+      // Pulling req.body name and bio and placing into Users.insert method
+      const newUser = await Users.insert({ name, bio })
+      // Sending the JSON response containing the newUser and a status 201 message
+      res.status(201).json(newUser)
+    }
+  } catch (err) {
+    res.status(500).json({
+      "message": "An error occured when posting a new user",
+      "error": err.message
+    })
+  }
 })
 
 // ===== SERVER EXPORT =====
